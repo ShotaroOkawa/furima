842b39240f29d8255e946cf0637f53cea25e0e9b
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-30 15:34:42 +0900

    一覧表示・詳細表示・編集機能・削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.all.order(id: 'DESC')
@@ -17,9 +18,30 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
-  def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delv_fee_id, :delv_from_id, :delv_time_id, :price).merge(user_id: current_user.id)
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :delv_fee_id, :delv_from_id, :delv_time_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
+
