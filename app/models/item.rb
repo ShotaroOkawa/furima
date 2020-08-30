a6d8fda1d2f3a55bffd21ce066f13d04277c5288
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-30 14:54:28 +0900

    JSで自動計算追加、モデルのテストコード追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,29 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delv_fee
+  belongs_to_active_hash :delv_from
+  belongs_to_active_hash :delv_time
   has_one_attached :image
+
+  belongs_to :user
+  has_one :item_management
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delv_fee_id
+    validates :delv_from_id
+    validates :delv_time_id
+  end
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :image
+  end
+
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }  # 価格が¥300~¥9,999,999であるか
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :family_name
+  end
+
+  with_options presence: true, format: { with: /\A[\p{katakana}ー－&&[^ -~｡-ﾟ]]+\z/, message: 'Full-width katakana characters' } do
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'includes both letters and numbers'
 end
