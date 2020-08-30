0591759da17232826c50527f88a0483150c9dd5b
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-30 17:11:05 +0900

    購買機能実装

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,48 @@
+class PurchasesController < ApplicationController
+  before_action :move_to_index, only: [:index, :create]
+  before_action :move_to_index2, only: [:index, :create]
+
+  def index
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    if @purchase_address.valid?
+      pay_item
+      @purchase_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+  def purchase_address_params
+    params.require(:purchase_address).permit(:price, :token, :zip_code, :prefecture_id, :city, :lot_number, :building_name, :phone_number).merge(user_id: current_user.id).merge(item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+    Payjp::Charge.create(
+      amount: purchase_address_params[:price],
+      card: purchase_address_params[:token], 
+      currency:'jpy'
+    )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    if user_signed_in?
+      redirect_to root_path if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def move_to_index2
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if Purchase.exists?(item_id: @item.id)
+  end
+end
---
+++ spec/models/purchase_address_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseAddress, type: :model do
+  describe '#create' do
+    before do
+      @purchase_address = FactoryBot.build(:purchase_address)
+    end
+
+    it 'numberとexp_month、exp_year、cvc, zip_code, prefecture_id、city、lot_number、building_name、telephoneが存在すれば登録出来ること' do
+      expect(@purchase_address).to be_valid
+    end
+
+    it 'クレジットカード情報は必須で、正しいカード情報が入力されていないと保存できないこと' do
+      @purchase_address.token = nil
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include "Token can't be blank"
+    end
+
+    it '郵便番号が空だと保存できないこと' do
+      @purchase_address.zip_code = nil
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include "Zip code can't be blank"
+    end
+
+    it '都道府県が空だと保存できないこと' do
+      @purchase_address.prefecture_id = nil
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include 'Prefecture Select'
+    end
+
+    it '市区町村が空だと保存できないこと' do
+      @purchase_address.city = nil
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include "City can't be blank"
+    end
+
+    it '番地が空だと保存できないこと' do
+      @purchase_address.lot_number = nil
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include "Lot number can't be blank"
+    end
+
+    it '電話番号が空だと保存できないこと' do
+      @purchase_address.phone_number = nil
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include "Phone number can't be blank"
+    end
+
+    it '郵便番号にハイフンがないと登録できないこと' do
+      @purchase_address.zip_code = '1111111'
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include 'Zip code Input correctly'
+    end
+
+    it '電話番号にハイフンがあると登録できないこと' do
+      @purchase_address.phone_number = '090-0000-0000'
+      @purchase_address.valid?
+      expect(@purchase_address.errors.full_messages).to include 'Phone number is invalid'
+    end
+
+    it '建物名が空でも登録できること' do
+      @purchase_address.building_name = ''
+      expect(@purchase_address).to be_valid
+    end
+
+  end
+end
