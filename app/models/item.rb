537fec548a6e7e643a53e11ab0b138cd8cc9296a
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-30 15:14:25 +0900

    商品一覧表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   belongs_to :user
-  has_one :item_management
+  has_one :purchase
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
