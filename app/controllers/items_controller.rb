8a1478490e87cc7dcb39bd97ad3a1d9287a354d6
ShotaroOkawa <shotaro.okawa@outlook.com>
2020-08-30 14:06:46 +0900

    コントローラー定義変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :delv_fee_id, :delv_from_id, :delv_time_id, :price).merge(user_id: current_user.id)
+  end
+
 end
